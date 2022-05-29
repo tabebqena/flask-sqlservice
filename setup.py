5e96e6f9a3530e76c14c1d7a54005a287bb73678
tabebqena@gmail.com <tabebqena@gmail.com>
2022-05-29 17:06:41 +0300

    update version & syncronize version

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+version = "1.0.0"
+with open("flasksqlservice2/__version__.py") as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith("__version__"):
+            version = line.split("=")[-1].strip().strip("\"'")
 setup(
     name='flask-sqlservice2',
-    # should match the package folder
     packages=['flasksqlservice2'],
-    version='1.0',
+    version=version,
     author='Ahmad Yahia',
     description='Flask extension for integration with sqlservice package > 2.0',
     keywords='flask, sql, extensions, sqlservice',
     python_requires='>=3.7, <4',
     install_requires=['sqlservice', 'flask'],
-    classifiers=[                                   
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Documentation',
