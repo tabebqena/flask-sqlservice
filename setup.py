afd3b5c153c34379b3ef697dc11cd04541108e48
tabebqena@gmail.com <tabebqena@gmail.com>
2022-05-29 15:22:53 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='flask-sqlservice',
+    # should match the package folder
+    packages=['flasksqlservice'],
+    version='1.0',
+    author='Ahmad Yahia',
+    description='Flask extension for integration with sqlservice package',
+    keywords='flask, sql, extensions, sqlservice',
+    python_requires='>=3.7, <4',
+    install_requires=['sqlservice', 'flask'],
+    classifiers=[                                   
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    entry_points={
+        'runners': [
+            'sample=sample:main',
+        ]
+    }
+)
